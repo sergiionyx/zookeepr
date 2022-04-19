a67e450f69dde468bdaf737b2b7ca25b18a5826d
sergii-onyx <twilight.cympak@gmail.com>
2022-04-19 12:33:15 -0400

    add get requests and connect css and js files

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const path = require('path');
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
+app.use(express.static('public'));
 const { animals } = require('./data/animals');
 
 function filterByQuery(query, animalsArray) {
@@ -100,15 +101,31 @@ app.get('/api/animals/:id', (req, res) => {
 app.post('/api/animals', (req, res) => {
     // set id based on what the next index of the array will be
     req.body.id = animals.length.toString();
-  
+
     // if any data in req.body is incorrect, send 400 error back
     if (!validateAnimal(req.body)) {
-      res.status(400).send('The animal is not properly formatted.');
+        res.status(400).send('The animal is not properly formatted.');
     } else {
-      const animal = createNewAnimal(req.body, animals);
-      res.json(animal);
+        const animal = createNewAnimal(req.body, animals);
+        res.json(animal);
     }
-  });
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
